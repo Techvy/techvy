25d2bbc94ef063ce5adb3873ad0c85048ec8bbb9
Techvy <techvyxd@gmail.com>
2025-04-03 18:19:09 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/techvy/',
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ['babel-plugin-styled-components', {
+            displayName: true,
+            fileName: false
+          }]
+        ]
+      }
+    })
+  ],
   server: {
     host: true, // Listen on all addresses
     port: 5173,
     allowedHosts: [
       '9f5b-152-58-85-179.ngrok-free.app'
     ]
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
   }
 })
