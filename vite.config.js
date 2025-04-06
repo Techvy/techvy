aa3529efd846a0a0b6112db83cd7ccb7b0a24bdb
Techvy <161228964+Techvy@users.noreply.github.com>
2025-04-06 21:17:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   base: '/techvy/',
   plugins: [
@@ -17,7 +15,7 @@ export default defineConfig({
     })
   ],
   server: {
-    host: true, // Listen on all addresses
+    host: true,
     port: 5173,
     allowedHosts: [
       '9f5b-152-58-85-179.ngrok-free.app'
